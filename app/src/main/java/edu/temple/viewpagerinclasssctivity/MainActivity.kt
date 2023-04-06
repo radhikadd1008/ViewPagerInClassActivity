e6119a5c90aa143337d44737d914e1a0d7a2bfb7
radhikadd1008 <radhikadd1008@gmail.com>
2023-04-06 15:21:38 -0400

    Worked with dynamic fragments using ViewPager.

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
@@ -12,12 +13,24 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.viewPager)
     }
 
+    private val addButton : Button by lazy {
+        findViewById(R.id.button)
+    }
+
+    var numberOfPages = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        addButton.setOnClickListener{
+            numberOfPages++
+            viewPager.adapter?.notifyItemInserted(numberOfPages-1)
+            viewPager.setCurrentItem(numberOfPages, false)
+
+        }
+
         viewPager.adapter = object : FragmentStateAdapter(this) {
-            override fun getItemCount() = 10
+            override fun getItemCount() = numberOfPages
 
             override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
 
